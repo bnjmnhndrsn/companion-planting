167a7a4e6c14f8415ea8d672336473bc8246de39
Ben <benjamin.c.henderson@gmail.com>
2015-07-12 13:01:09 -0400

    styling

--- app/models/planting.rb
+++ app/models/planting.rb
@@ -6,20 +6,6 @@ class Planting < ActiveRecord::Base
   validates :left, presence: true
   validates :right, presence: true
   validates :garden, presence: true
-  # validate :garden_square_must_not_exist, :garden_square_in_garden, on: :create
-
- #  def garden_square_must_not_exist
- #   if self.garden.garden_squares.where(column: self.column, row: self.row).any?
- #     errors.add(:garden, "garden square already exists")
- #   end
- #  end
- #
- # def garden_square_in_garden
- #   if (self.column < 0 || self.row < 0 || self.garden.width <= self.column ||
- #      self.garden.height <= self.row)
- #      errors.add(:garden, "garden square not in garden")
- #    end
- #  end
 
   def get_suggestions
     num = self.id % Plant.count
